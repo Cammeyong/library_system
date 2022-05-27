8cbfadf9c3d85c44d6c520879c7f2a69b56fe9f5
Cammeyong <cammeyon@gmail.com>
2022-05-26 21:21:44 -0500

    library_system

--- library_app/routes/student_register.js
+++ library_app/routes/student_register.js
@@ -16,7 +16,7 @@ router.get('/student_register', function(req,res,next){
     
 });
 
-router.post('/student_register/add', function(res,req,next){
+router.post('/student_register/add', function(req,res,){
     let data = {student_id:   req.body.student_id,
         first_nm:     req.body.first_nm,
         last_nm:      req.body.last_nm,
@@ -27,11 +27,14 @@ router.post('/student_register/add', function(res,req,next){
     let sqlQuery = "INSERT INTO library_system.students SET ?";
 
     conn.query(sqlQuery, data,(err, results) => {
-    if(err) 
-    console.log(err);
-    res.send(JSONResponse(results));
+    if(err) {
+        console.log(err);
+    } else {
+        res.redirect('/')
+    }
+    // res.send(JSONResponse(results));
     });
-    next();
+   
 })
 
 module.exports = router;
